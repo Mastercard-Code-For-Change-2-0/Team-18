896e13786cf9565be5785f705bbcfa58ea90d83c
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2025-08-25 13:11:57 +0530

    refactor: add links to add

--- client/src/components/NavLink.jsx
+++ client/src/components/NavLink.jsx
@@ -1,19 +1,20 @@
+import { Link } from "react-router-dom";
 export default function NavLink({ name, unlock }) {
 
-	// let to;
+	let to;
 
-	// if (name === "Register") to = "/register";
-	// else if (name === "") to = "/login";
-	// else to = null;
+	if (name === "Register") to = "/register";
+	else if (name === "") to = "/login";
+	else to = '/';
 
 	return (
 		unlock === "true" ?
-			<span className="p-3 font-medium text-[17px] text-gray-900">
-				{name}
-			</span>
-			// 	<Link to={to} className="p-3 font-medium text-[17px] hover:text-blue-500 transition">
+			// <span className="p-3 font-medium text-[17px] text-gray-900">
 			// 	{name}
-			// </Link>
+			// </span>
+			<Link to={to} className="p-3 font-medium text-[17px] hover:text-blue-500 transition">
+				{name}
+			</Link>
 			:
 			<span className="p-3 font-medium text-[17px] text-gray-400 cursor-not-allowed">
 				{name}
