ba90fd7aa2eead235b3caa7020ffc11821b97e1d
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2025-08-25 13:37:10 +0530

    feat: update UI styles and improve accessibility across components

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,19 +1,20 @@
+// NavBar.jsx
 import NavLink from "./NavLink";
 
 const NavBar = () => {
 	return (
-		<nav className="bg-white py-5 flex flex-col md:flex-row p-3 gap-2 justify-around items-center border-gray-200 border-b-3">
-			<p className="text-3xl font-extrabold">Y4D Foundation</p>
-			<div>
+		<nav className="bg-orange-50 py-5 flex flex-col md:flex-row p-3 gap-2 justify-around items-center border-b-3 border-orange-200">
+			<p className="text-3xl font-extrabold text-orange-700">Y4D Foundation</p>
+			<div className="flex flex-col md:flex-row gap-2">
 				<NavLink name="Home" unlock="true" />
-				<NavLink name="About Us" />
-				<NavLink name="Contact Us" />
-				<NavLink name="Donate" />
+				<NavLink name="About Us" unlock="true" />
+				<NavLink name="Contact Us" unlock="true" />
+				<NavLink name="Donate" unlock="true" />
 				<NavLink name="Register" unlock="true" />
 				<NavLink name="Login" unlock="true" />
 			</div>
 		</nav>
-	)
-}
+	);
+};
 
 export default NavBar;
