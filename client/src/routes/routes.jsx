49703b0d4cb3e12a033d61e5af390e347f035a0f
sutarrk9833 <itssanya123@gmail.com>
2025-08-25 11:33:09 +0530

    Added AdminBulkUpload page with drag-drop file upload & preview

--- client/src/routes/routes.jsx
+++ client/src/routes/routes.jsx
@@ -1,11 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
-
+import Admin from "../pages/Admin";
 import Home from "../pages/Home";
 const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <Home />,
 		// errorElement: <NotFound />,
+	},
+	{
+		  path:"/admin/bulk-upload",
+		  element:<Admin />
 	},
 	// {
 	// 	path: "shop",
